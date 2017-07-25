d3c938d8b81a810fd0cf8a74d8c98d8f124cd3d2
高文杰 <yidong-ios16@yhd.com>
2017-07-25 15:22:05 +0800

    header footer

---
+++ CTRefreshDemo/CTRefresh/CTRefreshFooterView.m
@@ -0,0 +1,21 @@
+//
+//  CTRefreshFooterView.m
+//  CTRefreshDemo
+//
+//  Created by wenjie on 2017/7/25.
+//  Copyright © 2017年 Demo. All rights reserved.
+//
+
+#import "CTRefreshFooterView.h"
+
+@implementation CTRefreshFooterView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
---
+++ CTRefreshDemo/CTRefresh/CTRefreshHeaderView.m
@@ -0,0 +1,21 @@
+//
+//  CTRefreshHeaderView.m
+//  CTRefreshDemo
+//
+//  Created by wenjie on 2017/7/25.
+//  Copyright © 2017年 Demo. All rights reserved.
+//
+
+#import "CTRefreshHeaderView.h"
+
+@implementation CTRefreshHeaderView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
