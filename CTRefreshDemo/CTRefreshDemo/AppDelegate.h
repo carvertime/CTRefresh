55d2e2391c2dcd6f02572d8335b46d23f70da1fb
高文杰 <yidong-ios16@yhd.com>
2017-07-25 15:15:22 +0800

    init

---
+++ CTRefreshDemo/CTRefreshDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CTRefreshDemo
+//
+//  Created by wenjie on 2017/7/25.
+//  Copyright © 2017年 Demo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CTRefreshDemo/CTRefreshDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CTRefreshDemo
+//
+//  Created by wenjie on 2017/7/25.
+//  Copyright © 2017年 Demo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
